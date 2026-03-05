305ad26bc0bba36806c3f99a792166420b9e7e05
analopez03 <analopezdeahumada97@gmail.com>
2026-03-05 12:30:45 +0100

    Subido y terminado el ejercicio 4 de JS

---
+++ Ejercicio4.js
@@ -0,0 +1,66 @@
+/*Ejercicio 4, refactoriza el código
+Dado los siguientes códigos, refactorízalos como consideres, si consideras que está bien, explica el motivo.
+
+Primer código
+Tenemos una función que saca el nombre completo del usuario con el nombre y apellido*/
+
+/// La variable usuario la he cambiado de var a const ya que siempre va a tener el mismo contenido.
+const usuario = {
+  nombre: "Pedro",
+  apellido: "Sánchez",
+  edad: 45,
+  profesion: "Barro man",
+};
+
+console.log(nombreUsuario(usuario));
+
+function nombreUsuario(user) {
+  const nombrCompleto = "Me llamo " + user.nombre + " " + user.apellido;
+
+  return nombrCompleto;
+}
+
+/*Segundo código
+Tenemos una función que calcula el salario anual y el nombre del usuario*/
+let user = {
+  name: "Pedro",
+  last: "Sánchez",
+  age: 45,
+  salary: 1000,
+  profesion: "Barro man",
+};
+
+function userData(user) {
+  const annualSalary = user.salary * 12;
+  const fullName = `${user.name} ${user.last}`;
+
+  return `Me llamo ${fullName} y cobro ${annualSalary}€ al año`;
+}
+
+console.log(userData(user));
+
+/*Tercer código
+Si el usuario tiene 30 años y no es español, recibirá una ayuda del gobierno de 2000€*/
+// Como la variable user ya ha sido declarada, he cambiado el valor de la variable en lugar de volver a declararla.
+user = {
+  name: "Álvaro",
+  last: "Morón",
+  age: 30,
+  nationality: "Morocco",
+};
+
+function verificarAyuda(user) {
+  // Cambiado el nombre de la función a algo más descriptivo.
+  const edad = 30; // Añadido para no dejar lo que se denomina números mágicos en el código.
+  if (user.nationality != "España") {
+    if (user.age == edad) {
+      return "Apto para la ayuda del gobierno";
+    } else {
+      return "No es apto para la ayuda del gobierno";
+    }
+  } else {
+    return "No es apto para la ayuda del gobierno";
+  }
+}
+
+console.log(verificarAyuda(user));
